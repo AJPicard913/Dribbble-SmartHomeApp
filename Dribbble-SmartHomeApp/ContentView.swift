3d49728d103991ad09664bea77415668cd61423c
ajpicard913 <Contact@saucedesigns.io>
2020-07-28 10:04:09 -0400

    Initial Commit

---
+++ Dribbble-SmartHomeApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Dribbble-SmartHomeApp
+//
+//  Created by AJ Picard on 7/28/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dribbble-SmartHomeApp/Dribbble_SmartHomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dribbble_SmartHomeAppApp.swift
+//  Dribbble-SmartHomeApp
+//
+//  Created by AJ Picard on 7/28/20.
+//
+
+import SwiftUI
+
+@main
+struct Dribbble_SmartHomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
